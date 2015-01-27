6e47b8095bca1084081d4b00b01d6eb095c2597d
Andy Rigby <andy.rigby@ixis.co.uk>
2015-01-27 17:28:34 +0000

    Intial proof of concept.

---
+++ README.md
@@ -0,0 +1,52 @@
+Drupal Drush
+===
+
+## A Codeception module for running drupal drush commands.
+
+_Drupal Drush_ is a [Codeception module](http://codeception.com/addons) for running drush commands on [Drupal](https://www.drupal.org/) sites.
+
+It also allows the use of the following statements in tests:
+
+```php
+// Execute "drush cc all"
+$output = $I->executeDrushCommand("cc", array("all"));
+```
+
+## Install with Composer
+
+```json
+{
+    "repositories": [
+        {
+            "type": "vcs",
+            "url": "git@bitbucket.org:dopey/codeception-drupal-drush.git"
+        }
+    ],
+    "require": {
+        "codeception/codeception": "2.0.*",
+        "ixisandyr/codeception-module-drupal-drush": "@dev"
+    }
+}
+```
+
+Drupal Drush minimally requires Codeception 2.0 and PHP 5.4
+
+## Example suite configuration
+
+```yaml
+class_name: AcceptanceTester
+modules:
+    enabled:
+        - PhpBrowser
+        - AcceptanceHelper
+        - DrupalDrush
+    config:
+        PhpBrowser:
+            url: 'http://localhost/myapp/'
+        DrupalDrush:
+            drush-alias: '@mysite.local' # The Drush alias to use (required).
+```
+
+### Required configuration
+
+* `drush-alias` is required.
--- src/DrupalDrush.php
+++ src/DrupalDrush.php
@@ -2,40 +2,73 @@
 
 namespace Codeception\Module;
 
-use Codeception\TestCase;
+use Codeception\Module;
 
-class DrupalDrush extends \Codeception\Module {
+class DrupalDrush extends Module {
     /**
      * @var array
      *   List of required config fields.
      */
     protected $requiredFields = array('drush-alias');
 
-    public function executeDrushCommand(array $arguments, $options = array())
+    /**
+     * Execute a drush command.
+     *
+     * @param string $command
+     *   Command to run.
+     *   e.g. "cc"
+     * @param array $arguments
+     *   Array of arguments.
+     *   e.g. array("all")
+     * @param array $options
+     *   Array of options in key => value format.
+     *   e.g. array("help" => null, "v" => null, "uid" => "2,3".
+     *
+     * @return string
+     *   Output from the drush command.
+     */
+    public function executeDrushCommand($command, array $arguments, $options = array(), $drush = 'drush', $return_var = 0)
     {
-        $command_args = array();
+        $command_args = array($command);
         foreach ($arguments as $arg) {
             $command_args[] = $arg;
         }
-
         foreach ($options as $opt => $value) {
-            if (isset($value)) {
-                $command_args[] = "--" . escapeshellarg($opt);
+            if (!isset($value)) {
+                if (strlen($opt) == 1) {
+                    $command_args[] = "-$opt";
+                } else {
+                    $command_args[] = "--$opt";
+                }
             } else {
-                $command_args[] = sprintf(
-                    "--%s=%s",
-                    $opt,
-                    escapeshellarg($value)
-                );
+                if (strlen($opt) == 1) {
+                    $command_args[] = sprintf(
+                        "-%s %s",
+                        $opt,
+                        escapeshellarg($value)
+                    );
+                } else {
+                    $command_args[] = sprintf(
+                        "--%s=%s",
+                        $opt,
+                        escapeshellarg($value)
+                    );
+                }
             }
         }
 
         $command = sprintf(
-            "drush -y %s %s",
+            "%s -y %s %s",
+            escapeshellcmd($drush),
             $this->config['drush-alias'],
-            implode(" ", array_map('escapeshellarg', $command_args));
+            implode(" ", array_map('escapeshellarg', $command_args))
         );
 
-        var_dump($command);
+        $output = array();
+        $this->debugSection('Command', $command);
+        exec($command, $output, $return_var);
+
+        $this->assertEquals(0, $return_var, "$command exited with code $return_var");
+        return $output;
     }
 }
